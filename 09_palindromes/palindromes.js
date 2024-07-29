a38c1ef485024cf8a288d83a21623a5902f8b6c1
Omar <omar.fayyaz@ryerson.ca>
2024-07-29 18:09:55 -0400

    finished 9. palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    const alphanumerics = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    const newArr = arr
+    .toLowerCase()
+    .split("")
+    .filter((char) => alphanumerics.includes(char))
+    .join("")
+    
+    const reverseString = newArr.split("").reverse().join("")
+
+    return reverseString === newArr
 };
-
 // Do not edit below this line
 module.exports = palindromes;
