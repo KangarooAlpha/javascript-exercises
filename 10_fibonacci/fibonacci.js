f4be1c2027f519b986e7ee16f39842ea50caee95
Omar <omar.fayyaz@ryerson.ca>
2024-07-30 19:07:19 -0400

    finished 10. fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let numA = (0);
+    let numB = (1);
+    let nTh = [];
+    if (num <= 2 && num >= 1) {
+        return 1;
+    }
+    else if (num == 0) {
+        return 0;
+    }
+    else if (num < 0) {
+        return ("OOPS");
+    }
+
+    for (let i = 1; i < num; i++) {
+        let total = ((numA + numB))
+        numA = numB
+        numB = total
+        //numB = nTh[nTh.lenght - 1] 
+    }
+    return numB
+    //return nTh[nTh.lenght - 1]
 
 };
 
